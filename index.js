77e5f2a4a59169b64a988fd263bcf505f7762c79
Bill Z. Qin <bzqin88@gmail.com>
2019-09-13 20:00:39 -0400

    framework (no dependencies)

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+var port = 8765;
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  res.render("index.html");
+});
+
+app.listen(process.env.PORT || port);
+console.log("We out here at port " + (process.env.PORT || port));
