a740ca9085e7ced1e723855dc5cb7880609d699b
Harvey Zheng <zheng.harvey5@gmail.com>
2019-09-14 16:22:38 -0400

    make bible smaller

--- ml/train_markov.py
+++ ml/train_markov.py
@@ -1,8 +1,8 @@
 import markovify
-corpus = open("./datasets/theoffice.txt").read()
+corpus = open("./datasets/bible.txt").read()
 
 text_model = markovify.Text(corpus, state_size=2)
 model_json = text_model.to_json()
 
-with open('./models/theoffice.json', 'w') as f:
+with open('./models/bible.json', 'w') as f:
     f = f.write(model_json)
