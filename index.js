9f47ac99c265dda0fe5b1c1e6a0cecded7d49cc9
Harvey Zheng <zheng.harvey5@gmail.com>
2019-09-13 22:40:25 -0400

    get html to work w/ node

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
+var path = require("path")
 
 var port = 8765;
 var app = express();
 
-app.use(express.static(__dirname + '/'));
-app.set('views', __dirname + '/frontend');
+app.use(express.static(path.join(__dirname,"public")));
+app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 var PYTHON_SCRIPT_NAME = "./filler.py";
 
-app.use(express.static('public'));
-
 app.get('/', function(req, res){
   res.render("index.html");
+});
+app.get('/impressionator', function(req, res){
+  res.render("impressionator.html");
 });
 
 app.post('/api', function(req,res){
